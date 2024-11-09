96727090d5e019f35862c9ff450d8a86cd7d8be0
Chaithanya K Nair <chaithanyanair06@gmail.com>
2024-11-09 21:21:16 +0530

    added the service call functionality

--- ToDoList/src/app/app.config.ts
+++ ToDoList/src/app/app.config.ts
@@ -5,6 +5,13 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+import { provideHttpClient, withInterceptorsFromDi, withFetch } from '@angular/common/http';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
+              provideRouter(routes), 
+              provideClientHydration(), 
+              provideAnimationsAsync(), 
+              provideHttpClient(withInterceptorsFromDi(), withFetch())
+  ]
 };
