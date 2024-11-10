4136fe3fad229d1725b463d98d3e9977f71e6d31
Chaithanya K Nair <chaithanyanair06@gmail.com>
2024-11-10 17:27:39 +0530

    added the integration

--- ToDoList/src/app/component/dashboard/dashboard.component.ts
+++ ToDoList/src/app/component/dashboard/dashboard.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { CrudService } from '../../services/crud.service';
 import { Task } from '../../model/task';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, FormsModule, CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
@@ -15,10 +20,15 @@ export class DashboardComponent {
   taskArr: Task[] = [];
 
   addTaskValue: string = '';
+  editTaskValue: string ='';
 
-  constructor(private crudService: CrudService) {}
+  constructor(private crudService: CrudService,
+              private snackBar: MatSnackBar) {}
 
   ngOnInit(): void {
+
+    this.addTaskValue = '';
+    this.editTaskValue = '';
     this.taskObj = new Task();
     this.taskArr =[];
     this.getAllTask();
@@ -30,29 +40,42 @@ export class DashboardComponent {
         this.taskArr = res;
       },
       error: (err) => {
-        alert("Unable to get the list of Tasks.");
+        // alert("Unable to get the list of Tasks.");/
+        this.snackBar.open('Unable to get the list of Tasks.', 'close',{
+          duration: 3000,
+        });
       },
     });
   }
 
-  addTask(etask: Task) {
-    this.crudService.addTask(etask).subscribe({
+  addTask() {
+    this.taskObj.task_name = this.addTaskValue 
+    this.crudService.addTask(this.taskObj).subscribe({
       next: (res) => {
         this.ngOnInit();
+        this.addTaskValue = '';
       },
       error: (err) => {
-        alert(err);
+        // alert(err);
+        console.error(err);
+        this.snackBar.open('An error occured', 'close',{
+          duration: 3000,
+        });
       },
     });
   }
 
   editTask() {
+    this.taskObj.task_name = this.editTaskValue;
     this.crudService.editTask(this.taskObj).subscribe({
       next: res =>{
         this.ngOnInit();
       },
       error: err => {
-        alert("Failed to edit the task");
+        // alert("Failed to edit the task");
+        this.snackBar.open('Failed to edit the task!', 'close',{
+          duration: 3000,
+        });
       }
     });
   }
@@ -63,8 +86,16 @@ export class DashboardComponent {
         this.ngOnInit();
       },
       error: err =>{
-        alert("Failed to delete the task.");
+        // alert("Failed to delete the task.");
+        this.snackBar.open('Failed to delete the task.', 'close',{
+          duration: 3000,
+        });
       }
     })
+  }
+
+  callTask(etask : Task){
+    this.taskObj = etask;
+    this.editTaskValue = etask.task_name;
   }
 }
